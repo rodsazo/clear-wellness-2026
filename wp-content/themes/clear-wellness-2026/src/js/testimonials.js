f015b67daa8d3750a922ff396bd9b6391b911721
Rodrigo Murillo <rodsazo@gmail.com>
2026-04-22 10:02:02 -0500

    testimonials

---
+++ wp-content/themes/clear-wellness-2026/src/js/testimonials.js
@@ -0,0 +1,51 @@
+jQuery(function ($){
+    $('.testimonials').each(function(i,el){
+
+        const $this = $(el);
+        const $sliderSubContent = $this.find('.testimonials__slider');
+        const $slides = $this.find('.testimonials__slide');
+        if ($slides.length <= 1) {
+            return;
+        }
+        const $angleBrackets = $this.find('.testimonials__control');
+        const $back = $angleBrackets.eq(0);
+        const $next = $angleBrackets.eq(1);
+        let currentIndex = 0;
+        resizeContainer( true );
+        $next.on('click',next);
+        $back.on('click',back);
+
+        // let interval = setInterval(next, 8000);
+
+        $(window).on('resize', function(){resizeContainer( false )});
+        $(window).on('load', function(){resizeContainer( true )});
+        $sliderSubContent.addClass('active');
+        function resizeContainer( firstLoad ){
+            if( firstLoad ) {
+                $sliderSubContent.height( $slides.eq( currentIndex ).outerHeight());
+            } else{
+                $sliderSubContent.animate({
+                    'height' : $slides.eq( currentIndex ).outerHeight()
+                });
+            }
+        }
+
+        function goto(index){
+            $slides.eq(currentIndex).fadeOut();
+            $slides.eq(index).css('display', 'grid').hide().fadeIn();
+            currentIndex = index;
+            resizeContainer();
+            clearInterval(interval);
+            interval = setInterval(next, 8000);
+        }
+
+        function next(){
+            const index = (currentIndex + 1)%$slides.length;
+            goto(index);
+        }
+        function back(){
+            const index = (currentIndex - 1+$slides.length)%$slides.length;
+            goto(index);
+        }
+    })
+});
