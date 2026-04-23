a789b2372f86f7bf10d8264e14de520f23fe13f9
Rodrigo Murillo <rodsazo@gmail.com>
2026-04-23 08:20:15 -0500

    partial

---
+++ wp-content/themes/clear-wellness-2026/parts/blocks/video-block.php
@@ -0,0 +1,14 @@
+<?php
+
+$videoIframe = get_field('video_iframe');
+$videoImage = get_field('video_image');
+$title = get_field('title');
+$text = get_field('text');
+
+?>
+
+<div class="container">
+    <div class="videoBlock">
+
+    </div>
+</div>
