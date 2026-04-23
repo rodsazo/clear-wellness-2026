4212267edd422f24c0533e538c4fdfec716f536a
Rodrigo Murillo <rodsazo@gmail.com>
2026-04-23 11:35:20 -0500

    content block

---
+++ wp-content/themes/clear-wellness-2026/parts/blocks/content-cards-block.php
@@ -0,0 +1,20 @@
+<?php
+$columns = get_field('columns') ?: '3';
+$cards = get_field('cards') ?: [];
+?>
+
+<div class="container">
+    <div class="contentCards contentCards--cols-<?= esc_attr($columns); ?>">
+        <?php foreach( $cards as $card ):
+            $content = $card['content'];
+            ?>
+            <div class="contentCards__item | intersect fadeIn">
+                <?php if( $content ): ?>
+                    <div class="wysiwyg t-trim">
+                        <?= $content; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
