fa7ff4dc512bb6db5a51f6651e3b3b5930330adb
Rodrigo Murillo <rodsazo@gmail.com>
2026-05-10 20:52:36 -0500

    fix content cards

--- wp-content/themes/clear-wellness-2026/parts/blocks/centered-video-block.php
+++ wp-content/themes/clear-wellness-2026/parts/blocks/centered-video-block.php
@@ -9,12 +9,12 @@ $videoId = uniqid();
     <div class="centeredVideo">
         <div class="centeredVideo__thumb">
             <?= wp_get_attachment_image( $video_image, 'large' ); ?>
-            <a class="centeredVideo__playBtn" href="#modal-<?= $videoId; ?>" aria-label="Play video">
+            <button class="centeredVideo__playBtn" aria-haspopup="dialog" data-modal="modal-<?= $videoId; ?>" aria-label="Play video">
                 <span class="centeredVideo__playIcon"></span>
                 <?php if( $button_text ): ?>
                     <span class="centeredVideo__label"><?= esc_html( $button_text ); ?></span>
                 <?php endif; ?>
-            </a>
+            </button>
         </div>
     </div>
 </section>
--- wp-content/themes/clear-wellness-2026/parts/blocks/content-cards-block.php
+++ wp-content/themes/clear-wellness-2026/parts/blocks/content-cards-block.php
@@ -4,17 +4,17 @@ $cards = get_field('cards') ?: [];
 ?>
 
 <section class="container">
-    <div class="contentCards contentCards--cols-<?= esc_attr($columns); ?>">
+    <ul class="contentCards contentCards--cols-<?= esc_attr($columns); ?>">
         <?php foreach( $cards as $card ):
             $content = $card['content'];
             ?>
-            <div class="contentCards__item | intersect fadeIn">
+            <li class="contentCards__item | intersect fadeIn">
                 <?php if( $content ): ?>
                     <div class="wysiwyg t-trim">
                         <?= $content; ?>
                     </div>
                 <?php endif; ?>
-            </div>
+            </li>
         <?php endforeach; ?>
-    </div>
+    </ul>
 </section>
