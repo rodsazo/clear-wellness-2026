f32ca7257130f0ca54537f0cdd5bf7129e7ea0d1
Rodrigo Murillo <rodsazo@gmail.com>
2026-05-13 11:37:58 -0500

    page hero supports accent

--- wp-content/themes/clear-wellness-2026/parts/blocks/page-hero-block.php
+++ wp-content/themes/clear-wellness-2026/parts/blocks/page-hero-block.php
@@ -1,13 +1,19 @@
 <?php
 
 $title = get_field('title');
+$accent = get_field('accent');
 $text = get_field('text');
 $bid = blockId();
 ?>
 
 <header class="container container--886 text-center" aria-labelledby="<?= $bid; ?>-title">
     <div class="flow">
-        <h1 class="t-h1 t-trim" id="<?= $bid; ?>-title"><?= $title; ?></h1>
+        <h1 class="t-h1 t-trim" id="<?= $bid; ?>-title">
+            <?= $title; ?>
+            <?php if( $accent ): ?>
+                <span class="t-accent"><?= $accent; ?></span>
+            <?php endif; ?>
+        </h1>
 
         <?php if( $text ): ?>
             <div class="t-large">
